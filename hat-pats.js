14d55e22a7fc0fda9253f33837a5acc036a45063
Charles Holbrow <CharlesHolbrow@gmail.com>
2021-02-01 23:35:01 -0500

    ok

--- hat-pats.js
+++ hat-pats.js
@@ -1,10 +1,39 @@
-const h0 = {
-  r:   '1 + 2 + ..3 + 4 + ',
-  hat: 'hhhhhhh hhhhhhhhhh'
-}
-const h1 = {
-  r:   '1 + 2 + ..3 + 4 + ',
-  hat: 'h h h h hhhhhhhhhh',
-}
+const pats = [
+  {
+    r:   '1 + 2 +...3 + 4.e.+ ',
+    hat: 'hohohoh hhhohohoh-ho'
+  },
+  {
+    r:   '1 + 2 +...3 + 4 + ',
+    hat: 'h h h h hohohohoho',
+  },
+  {
+    r:   '1 e + a 2 e + a 3.....e...+..a.4 e + a ',
+    hat: 'hohohohohohoh h hohohhohohohohoh o h o ',
+  },
+  {
+    r:   '1 e + a 2 e + a..3.....e + a 4 e + a ',
+    hat: 'h   h   h h hohoohoohoohohohohohohoho',
+  },
+]
 
-module.exports = [h0, h1]
+const variations = [
+  {
+    r:   '1 + 2 +...3 + 4.e.+ ',
+    hat: 'hohoh     hohohoh-ho'
+  },
+  {
+    r:   '1 + 2 +...3 + 4 + ',
+    hat: 'h h h h hohohohoho',
+  },
+  {
+    r:   '1 e + a 2 e + a 3.....e...+..a.4 e + a ',
+    hat: 'hohohohoh       hohohhohohohohoh o h o ',
+  },
+  {
+    r:   '1 e + a 2 e + a..3.....e + a 4 e + a ',
+    hat: 'h   h   h h hohoohoohooh o h o h hoho',
+  },
+]
+
+module.exports = { pats, variations }
--- index.js
+++ index.js
@@ -12,10 +12,15 @@ const trapKick = copy(kit.tLibrary.D, {
   pitchSemitones: -1.59 // this just sounds good ¯\_(ツ)_/¯
 })
 // timing adjustments
+const clap = trapKit.p
 trapKit.B = new techniques.AFOnset(trapKick, 0.01)
-trapKit.p = new techniques.AFOnset(trapKit.p, 0.03)
+trapKit.p = new techniques.AFOnset(clap, 0.03)
+trapKit.o = copy(trapKit.h, { gainDb: -3.5 })       // offbeat hi-hat
+trapKit.r = new techniques.AFReverse(clap, 0.03)    // reverse clap
 
 // bass fades in
+tr808.bass.b = copy(tr808.bass.g, { pitchSemitones: 4 })
+tr808.bass.B = copy(tr808.bass.G, { pitchSemitones: 4 })
 tr808.bass.f = copy(tr808.bass.g, { fadeInSeconds: 0.07 })
 tr808.bass.F = copy(tr808.bass.G, { fadeInSeconds: 0.07 })
 
@@ -36,48 +41,78 @@ const bassComp = new plugins.RoughRider3Vst2({
   mixPercent: 94,
 })
 
+const mainComp = new plugins.RoughRider3Vst2({
+  ratio: 1.5,
+  sensitivityDb: -27,
+  makeupGainDb: 0,
+  attackMs: 2,
+  mixPercent: 50,
+  releaseMs: 350,
+})
+
+const verb = new plugins.DragonflyRoomVst2({
+  dryLevelPercent: 0,
+  decaySeconds: 1.8,
+  lowBoostPercent: 0,
+})
+
+const delay16 = new plugins.ValhallaFreqEchoVst2({
+  wetDryPercent: 100,
+  sync: plugins.ValhallaFreqEchoVst2.parameterLibrary.sync.choices['1/16'],
+})
+
 // Create a tLibrary filled with MIDI Chords
 const chordLibrary = {
-  a: new techniques.MidiChord({ notes: [64, 67, 71] }), // e minor
-  b: new techniques.MidiChord({ notes: [64, 67, 69] }),
-  c: new techniques.MidiChord({ notes: [59, 66, 69] }),
+  b: new techniques.MidiChord({ notes: [59, 64, 71] }),
   d: new techniques.MidiChord({ notes: [59, 64, 67] }),
-  e: new techniques.MidiChord({ notes: [59, 62, 64] }),
+  A: new techniques.MidiChord({ notes: [47, 50, 59] }),
+  B: new techniques.MidiChord({ notes: [47, 52, 62] }),
 }
 
 // Instantiate a Podolski VST2 plugin from a preset
 const padSynthA = plugins.podolskiVst2Presets.brightPad()
-const padSynthB = plugins.podolskiVst2Presets.brightPad()
+padSynthA.parameters.dly1CrossfeedPercent = 60
+padSynthA.parameters.dly1MixPercent = 33
 padSynthA.parameters.vcf0Cutoff = 22
+
+const padSynthB = plugins.podolskiVst2Presets.brightPad()
 padSynthB.parameters.vcf0Cutoff = 2
+padSynthB.parameters.dly1MixPercent = 12
 
 const score = {
   r:      '1 + 2 + 3 + 4 + ',//1 + 2 + 3 + 4 + '
-  snare: ['    K       K   ', '    K       K   '],
-  clap:  ['    p       p   ', '    p       p   '],
-  kick:  ['B  B         B B', 'B  B B   B B    '],
-  bass:  ['g--          G--'],
-  hat:   [hatPats[0],         hatPats[1]],
-  // padA:  ['a-b---- ', 'c--c----', 'd---    ', '        '],
-  // padB:  ['        ', '        ', '        ', 'e-------'],
+  snare: ['    K       K   ', '    K       K   ', '    K       K   ', '    K       K   '],
+  clap:  ['    p       p   ', '    p       p-r-', '    p       p   ', '    p       p-r-'],
+  kick:  ['B  B         B B', 'B  B B   B B    ', 'B  B         B B', 'B  B B   B B    '],
+  bass:  ['g-------     B--', 'g-------        ', 'g-------     B--', 'g-------     B--'],
+  hat:   hatPats.variations,
+  pads: {
+    r: '1 2 3 4 ',
+    padA:  ['        ', 'b-      ', '        ', 'd-      '],
+    padB:  ['A-      ', 'B-      ', 'A---    ', 'B---    '],
+  }
 }
 
 // Create a Session, specifying beats-per-minute and track configuration
-const session = new FluidSession({ bpm: 78, loopStartTime: 1, loopDuration: 2 }, [
-  // In the first example, we specified a tLibrary in the score object. In this
-  // example, tLibrary objects are specified in the track objects.
-  { name: 'drums', gainDb: -6, tLibrary: trapKit, children: [
-    { name: 'snare', gainDb: -3 },
-    { name: 'clap' },
-    { name: 'kick', gainDb: -4 },
-    { name: 'hat' },
-  ]},
-  { name: 'bass', gainDb: -12, plugins: [bassComp], tLibrary: tr808.bass },
-  // Notice the pad tracks have a .plugins array containing the synthesizer
-  // preset. It also has a dedicated tLibrary containing MIDI chords.
-  { name: 'pads', tLibrary: chordLibrary, plugins: [ducker], children: [
-    { name: 'padA', plugins: [padSynthA] },
-    { name: 'padB', plugins: [padSynthB] },
+const session = new FluidSession({ bpm: 63, loopStartTime: 1, loopDuration: 4 }, [
+  { name: 'main', plugins: [mainComp], children: [
+    // In the first example, we specified a tLibrary in the score object. In this
+    // example, tLibrary objects are specified in the track objects.
+    { name: 'drums', gainDb: -6, tLibrary: trapKit, children: [
+      { name: 'snare', gainDb: -3, sends: [{ to: 'verb', gainDb: 0 }]  },
+      { name: 'clap', sends: [{ to: 'verb', gainDb: 0 }] },
+      { name: 'kick', gainDb: -4, sends: [{ to: 'verb', gainDb: -8 }]  },
+      { name: 'hat', gainDb: -10 },
+    ]},
+    { name: 'bass', gainDb: -12, plugins: [bassComp], tLibrary: tr808.bass },
+    // Notice the pad tracks have a .plugins array containing the synthesizer
+    // preset. It also has a dedicated tLibrary containing MIDI chords.
+    { name: 'pads', gainDb: -8, tLibrary: chordLibrary, sends: [{ to: 'verb', gainDb: 0 }],  plugins: [ducker], children: [
+      { name: 'padA', gainDb: -7, plugins: [padSynthA] },
+      { name: 'padB', gainDb: 0, plugins: [padSynthB] },
+    ]},
+    { name: 'verb', gainDb: -15, plugins: [verb] },
+    { name: 'delay16', gainDb: 0, plugins: [delay16] },
   ]}
 ])
 
